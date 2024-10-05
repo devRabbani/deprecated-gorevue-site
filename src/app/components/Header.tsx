2db4ffa51c05ffd650d99e55f6243ff3372b8809
Julkarnain Shahid <jshahid1997@gmail.com>
2024-10-05 12:48:50 +0530

    feat: added header, hero section, vision section, help section, footer, and qr scan page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,8 +3,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
-import { auth } from "../lib/firebase";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../contexts/AuthContext";
 import { signIn, signOut } from "../lib/firebase";
